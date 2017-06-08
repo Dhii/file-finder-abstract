7d56c03789b91a67a1a98f05bb770b05f109e0d1
Anton Ukhanev <xedin.unknown@gmail.com>
2017-06-08 16:19:40 +0200

    Added abstract finder

---
+++ src/Exception.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dhii\File\Finder;
+
+/**
+ * An exception that is related to file finding.
+ *
+ * @since [*next-version*]
+ */
+class Exception extends \Exception implements ExceptionInterface
+{
+}
